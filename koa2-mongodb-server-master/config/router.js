41b8ce1ab1df3f96480f8d339b77d08eb2297adf
wangjiafenghw <wang_jiafeng@yahoo.com>
2018-11-30 12:20:42 +0800

    deldemo

--- koa2-mongodb-server-master/config/router.js
+++ koa2-mongodb-server-master/config/router.js
@@ -3,8 +3,6 @@
 const Router = require('koa-router')
 const User = require('../app/controllers/user')
 const App = require('../app/controllers/app')
-const fs = require('fs')
-const path = require("path")
 
 module.exports = function(){
 	var router = new Router({
@@ -16,10 +14,6 @@ module.exports = function(){
   router.post('/u/signup', App.hasBody, User.signup)
   router.post('/u/update', App.hasBody, App.hasToken, User.update)
 
-  // DB Interface test
-  router.get('/test/user/users',User.users)
-  router.post('/test/user/add',User.addUser)
-  router.post('/test/user/delete',User.deleteUser)
 
   return router
 }
